a1bb4758b583b890207f3422987039444dbc998a
ZoieBoyd <zoieboyd@outlook.com>
2026-07-15 18:08:28 +0100

    fix: Fix styling issue on wider mobile screens
    Fixed styling issue whereby hidden nav menu (on mobile) would be slightly visible on wider phone displays.

--- src/script.js
+++ src/script.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 const navMenu = document.querySelector("nav");
 const hamburgerBtn = document.querySelector(".hamburger-btn");
-const navOptions = document.querySelectorAll("nav ul li a");
+const navOptions = document.querySelectorAll(".nav-option");
 
-if (screen.width < 600) {
+if (screen.width <= 600) {
     hamburgerBtn.addEventListener("click", () => {
         navMenu.classList.toggle("active");
         hamburgerBtn.classList.toggle("active");
